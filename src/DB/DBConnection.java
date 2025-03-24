0295bd0058d5055a01213e93f3b05107421655b6
unknown <brianryan215@gmail.com>
2025-03-24 19:23:25 +0300

    modified mysql connection

--- src/DB/DBConnection.java
+++ src/DB/DBConnection.java
@@ -2,10 +2,13 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ *//*
+
 package DB;
 
-/***** DBConnection.java *****/  
+*/
+/***** DBConnection.java *****//*
+
  import java.sql.Connection;  
  import java.sql.DriverManager;  
  import java.sql.SQLException;  
@@ -42,4 +45,31 @@ public class DBConnection {
      connect();  
      return conn;  
    }  
- }   
+ }   */
+
+
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static Connection conn;
+    public static String url = "jdbc:mysql://localhost:3306/bank?useSSL=false";
+    private static String user = "root"; // Username of database
+
+    public static Connection connect() throws SQLException {
+        // Remove manual loading of the driver class as it is done automatically in newer versions
+        conn = DriverManager.getConnection(url, user, "");
+        return conn;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        if (conn != null && !conn.isClosed()) {
+            return conn;
+        }
+        connect();
+        return conn;
+    }
+}
